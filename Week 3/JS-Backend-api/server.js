76ad78bd779bb7075c93183fa0f9a945ebefa69e
peculiar okon <peculiargirl4real@gmail.com>
2026-05-22 16:59:30 +0100

    Added frontend

--- Week 3/JS-Backend-api/server.js
+++ Week 3/JS-Backend-api/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const connectDB = require("./config/db");
 
 const userRoutes = require("./Routes/UserRoutes");
 
@@ -8,6 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/api", userRoutes);
+connectDB();
 
 app.get("/", (req, res) => {
   res.send("API is running");
